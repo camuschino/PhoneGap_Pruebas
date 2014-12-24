99a9f3ecd59fa3433b25ff2d86c336f68ebbcee2
Emmanuel <emmanuel_cabral@hotmail.com>
2014-12-24 11:33:41 -0300

    commit_6

--- platforms/android/assets/www/js/function.js
+++ platforms/android/assets/www/js/function.js
@@ -1,18 +1,42 @@
-function button() {
-    alert("HOLAAAAA");
+function button(){
+
+    alert("asdf");
 }
 
 function create_folder(){
 
-// request the persistent file system
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onSuccess, onError);
+    window.requestFileSystem(window.TEMPORARY, 5*1024*1024 /*5MB*/, onInitFs, errorHandler);
 
-    function onSuccess(fileSystem) {
-        console.log(fileSystem.name);
+    function onInitFs(fs) {
+
+        alert('Opened file system: ' + fs.name);
     }
 
-    function onError(Cod_error) {
-        console.log(Cod_error.code);
+    function errorHandler(e) {
+        var msg = '';
+
+        switch (e.code) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'QUOTA_EXCEEDED_ERR';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'NOT_FOUND_ERR';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'SECURITY_ERR';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'INVALID_MODIFICATION_ERR';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'INVALID_STATE_ERR';
+                break;
+            default:
+                msg = 'Unknown Error';
+                break;
+        };
+
+        console.log('Error: ' + msg);
     }
 
 }
